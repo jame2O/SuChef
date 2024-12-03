4fe82f117b2d0fadad9f3fbac4ebbb4f29278ae0
james2O <jamowaind@gmail.com>
2024-12-03 18:37:54 +0000

    changed some small things

--- api/util/shopping_list.py
+++ api/util/shopping_list.py
@@ -3,18 +3,23 @@ import json
 import pandas as pd
 import numpy as np
 
-def calculate():
+def get_quantities():
+    
+    ## Declare the recipe dataframe and 
     recipe_df = pd.DataFrame({})
-    with open("./api/data/recipe_data.json") as f:
+    with open("./MealPrep/api/data/recipe_data.json") as f:
         recipe_df = pd.read_json(f)
-
     f.close()
     recipe_df = pd.read_json(json.dumps(recipe_df.data[0]))
-    test_ingredients = pd.json_normalize(recipe_df.ingredients[0])
-    
+
     total_quants = {}
     
-    for index, row in test_ingredients.iterrows():
+    for i, r in recipe_df.iterrows():
+        ingredients = pd.json_normalize(r.ingredients)
+        print(ingredients)
+        
+    
+    for index, row in ingredients.iterrows():
         ing_name = row["name"]
         print(row["quantity.quantity"])
         ing_quantity = 0
@@ -26,7 +31,12 @@ def calculate():
             ing_quantity = row["quantity"]
         if ing_name in total_quants:
             total_quants[ing_name].quantity += ing_quantity
-        
+        else:
+            total_quants[ing_name] = {
+                    "quantity": row["quantity.quantity"],
+                    "unit": row["quantity.unit"]
+                }
+    return total_quants
 if __name__ == '__main__':
-    calculate()
+    print(get_quantities())
         
